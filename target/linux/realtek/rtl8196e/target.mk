290eb9b9f66b9acec864dd360f857e1fb1613381
Zafar Iqbal <zafar.amt@gmail.com>
2022-03-31 00:44:29 +0530

    initial commit

---
+++ target/linux/realtek/rtl8196e/target.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2019 OpenWrt.org
+#
+
+SUBTARGET:=rtl8196e
+BOARDNAME:=RTL8196e based boards
+ARCH_PACKAGES:=realtek_lx43
+CPU_TYPE:=lx43
+
+define Target/Description
+        Build firmware images for Realtek RTL8196E based boards.
+endef
+
+
---
+++ target/linux/realtek/rtl8197f/target.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2019 OpenWrt.org
+#
+
+ARCH:=mipsel
+SUBTARGET:=rtl8197f
+BOARDNAME:=RTL8197f based boards
+CPU_TYPE:=24kc
+
+define Target/Description
+        Build firmware images for Realtek RTL8197F based boards.
+endef
+
+
